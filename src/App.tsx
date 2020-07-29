d9253eeb2bdde1fa811cc2c6bbdf499c768c687b
吴灏 <wuh@cnlemon.net>
2020-07-29 23:45:47 +0800

    样式基本完成，还剩余一个剩余参数

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,17 @@ import Button from './components/Button';
 
 function App() {
   return (
-    <div className='App'>
-      <Button>默认样式</Button>
-    </div>
+    <>
+      <br /><br />
+      <Button btnType='default' disabled>默认样式 disabled</Button>
+      <br /><br />
+      <Button btnType='primary' size='lg'>primary lg</Button>
+      <br /><br />
+      <Button btnType='danger' size='sm'>danger sm</Button>
+      <br /><br />
+      <Button btnType='link'>link按钮</Button>
+      <br /><br />
+    </>
   );
 }
 
