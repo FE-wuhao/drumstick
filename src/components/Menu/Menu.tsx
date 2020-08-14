53294dbbcfb10be578c5ebf020c1c793befb06ad
吴灏 <wuh@cnlemon.net>
2020-08-14 17:53:48 +0800

    menu又修改了一点

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React,{createContext,useState} from 'react';
 import classNames from 'classnames';
 import { Key } from '../../typings/typings';
 
 type dir = 'horizontal' | 'vertical';
+type MenuCallback = (key: Key) => void;
 
 interface IMenu {
   key: Key;
   className: string;
   direction: dir;
   defaultHighlightKey: Key;
-  onSelected: (key: Key) => void;
+  onSelected: MenuCallback;
 }
+
+interface IMenuContext{
+  highlightKey: Key;
+  getSelected?: MenuCallback;
+}
+
+export const MenuContext = createContext<IMenuContext>({highlightKey:0});
 
 const Menu: React.FC<Partial<IMenu>> = (props) => {
   const { children, className, direction, defaultHighlightKey } = props;
 
+  const [currentKey, setCurrrentKey] = useState<Key>();
+
   const classes = classNames(className, `Menu-${direction}`);
 
-  return <ul className={classes}>{children}</ul>;
+  const currentKeyValue = { highlightKey: currentKey, getSelected:setCurrrentKey};
+
+  return (
+    // <MenuContext.Provider value={currentKeyValue}>
+      <ul className={classes}>{children}</ul>
+    // </MenuContext.Provider>
+  );
+  
 };
 
 Menu.defaultProps = {
